af3527045bb0a34f4d7b8361bf0c24a6fc610722
jaspreet42 <50900127+jaspreet42@users.noreply.github.com>
2019-07-18 13:04:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -81,3 +81,25 @@ $updater = new Smashing_Updater( __FILE__ ); // instantiate our class
 $updater->set_username( 'jaspreet42' ); // set username
 $updater->set_repository( 'bsd-structure-data' ); // set repo
 $updater->initialize(); // initialize the updater
+
+public function modify_transient( $transient ) {
+
+  if( property_exists( $transient, 'checked') ) { // Check if transient has a checked property
+    if( $checked = $transient->checked ) { // Did WordPress check for updates?
+      $this->get_repository_info(); // Get the repo info
+      $out_of_date = version_compare( $this->github_response['tag_name'], $checked[$this->basename], 'gt' ); // Check if we're out of date
+      if( $out_of_date ) {
+        $new_files = $this->github_response['zipball_url']; // Get the ZIP
+        $slug = current( explode('/', $this->basename ) ); // Create valid slug
+        $plugin = array( // setup our plugin info
+          'url' => $this->plugin["PluginURI"],
+          'slug' => $slug,
+          'package' => $new_files,
+          'new_version' => $this->github_response['tag_name']
+        );
+        $transient->response[ $this->basename ] = (object) $plugin; // Return it in response
+      }
+    }
+  }
+  return $transient; // Return filtered transient
+}
